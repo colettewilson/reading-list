b781acbeb6b149adf8a743fb6840f226c7db6ecf
Colette Wilson Wyatt <colette.wilson-wyatt@ltgplc.com>
2020-07-10 11:29:39 +0100

    get data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from "axios"
 import styles from "./App.module.css"
 import Header from "./components/Header"
 import Loader from "./components/Loader"
 
 function App() {
+  const [{isLoading, bookList}, setBookList] = useState({
+    isLoading: true,
+    bookList: []
+  })
+  const url = `http://localhost:3000/data.json`
+  const CancelToken = axios.CancelToken;
+  const source = CancelToken.source()
+
+  const fetchData = async() => {
+    await axios.get(url, {
+      cancelToken: source.token
+    })
+      .then(response => {
+        setBookList({
+          isLoading: false,
+          bookList: response.data.books
+        })
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+
+    return () => {
+      source.cancel('Operation canceled by the user.')
+    }
+  }, [])
+
+  console.log(isLoading, bookList)
+
   return (
     <div className={styles.app}>
       <Header />
