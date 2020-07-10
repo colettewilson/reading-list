d06614fe6ebd7ef93aa5a2ce9cc5c31ee4a06da0
Colette Wilson Wyatt <colette.wilson-wyatt@ltgplc.com>
2020-07-10 11:21:14 +0100

    show loading anim before data fetched

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import styles from "./App.module.css"
 import Header from "./components/Header"
+import Loader from "./components/Loader"
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <Loader />
     </div>
   )
 }
---
+++ src/components/Loader.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styles from "./Loader.module.css"
+
+const Loader = () => {
+  return (
+    <div className={styles.loader}>
+      <span className={styles.loaderItem}></span>
+      <span className={styles.loaderItem}></span>
+      <span className={styles.loaderItem}></span>
+      <p>Loading</p>
+    </div>
+  )
+}
+
+export default Loader
