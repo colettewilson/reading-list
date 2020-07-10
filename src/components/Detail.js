d0b449bc5a0b01958b1148a9fb0cabe4af45b28b
Colette Wilson Wyatt <colette.wilson-wyatt@ltgplc.com>
2020-07-10 11:55:19 +0100

    add detail view

---
+++ src/components/Detail.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import styles from "./Detail.module.css"
+
+const Detail = (props) => {
+  return (
+    <article className={styles.detail}>
+      {props.cover && <aside className={styles.detailImage}>
+        <img src={props.cover} alt={`${props.name ? props.name : "Book"} cover`} />
+      </aside>}
+      <main className={styles.detailMain}>
+        <div className={styles.detailTitle}>
+          {props.name && <h1>{props.name}</h1>}
+          {props.author && <p>By {props.author}</p>}
+        </div>
+        {props.isbn && <p>ISBN: {props.isbn}</p>}
+      </main>
+      <footer>
+        <Link to="/">Back to reading list</Link>
+      </footer>
+    </article>
+  )
+}
+
+export default Detail
--- src/pages/BookPage.js
+++ src/pages/BookPage.js
@@ -1,14 +1,25 @@
 import React from "react"
+import { connect } from "react-redux"
+import { slugify } from "../helpers/slugify"
 import Header from "../components/Header"
 import Loader from "../components/Loader"
+import Detail from "../components/Detail"
 
 const BookPage = (props) => {
   return (
     <>
       <Header />
-      <Loader />
+      {props.isLoading ? <Loader /> : <Detail {...props.book} />}
     </>
   )
 }
 
-export default BookPage
+const mapStateToProps = (state, ownProps) => {
+  let slug = ownProps.match.params.bookSlug
+  return {
+    isLoading: state.books.isLoading,
+    book: state.books.bookList.find(b => slugify(b.name) === slug)
+  }
+}
+
+export default connect(mapStateToProps, null)(BookPage)
